adf7f46b5602bb868277d577bafb5796fd058e6d
Coralie Pereira <coralie-p@outlook.fr>
2023-03-14 14:58:50 +0100

    soucoupe dans grille

--- app.js
+++ app.js
@@ -26,42 +26,136 @@ button.onclick = function createVaisseaux() {
 
     
 
+   
 
 
-// On prend l'élément HTML avec l'id de la soucoupe
-// const soucoupe = document.getElementById('case-229');
-
-// // Position de base de la soucoupe
-// let positionsoucoupe = 0;
-
-// // la fonction pour déplacer la soucoupe sur la gauche
-// function deplacersoucoupeGauche() {
-//   positionsoucoupe -= 10; // pour bouger la soucoupe de 10 px vers la gauche
-//   soucoupe.style.left = positionsoucoupe + 'px'; // pour la maj de la position de la soucoupe
-// }
-
-// // la fonction pour bouger la soucoupe vers la droite
-// function deplacersoucoupeDroite() {
-//   positionsoucoupe += 10; // bouger la soucoupe de 10 px sur la droite
-//   soucoupe.style.left = positionsoucoupe + 'px'; // maj de la position de la soucoupe
-// }
-
-// //  les événements clavier pour déplacer la soucoupe
-// document.addEventListener('keydown', (event) => {
-//   if (event.key === 'ArrowLeft') {
-//     deplacersoucoupeGauche();
-//   } else if (event.key === 'ArrowRight') {
-//     deplacersoucoupeDroite();
-//   }
-// });
-
-
+   
+   
+    // let x = 230; // position de base du tireur
+    // document.addEventListener('keyup', function (e) {
+    //     let tireurElement = document.querySelector('div.tireur');
+    //     if (e.key === 'ArrowLeft'|| e.keyCode == 37) {
+    //         // bouger le tireur vers la gauche
+    //         if (x != 221 && x != 201 && x != 181) {
+    //             position = x;
+    //             tireurElement.classList.remove('tireur');
+    //             x--;
+    //             document.getElementById('case-' + x).classList.add('tireur');
+    //             position = x;
+    //             console.log(position)
+    //         }
+    //     }
+    //     if (e.key === 'ArrowRight' || e.keyCode == 39) {
+    //         // bouger le tireur vers la droite
+    //         if (x != 240 && x != 220 && x != 200) {
+    //             position = x;
+    //             tireurElement.classList.remove('tireur');
+    //             x++;
+    //             document.getElementById('case-' + x).classList.add('tireur');
+    //             position = x;
+    //             console.log(position)
+    //         }
+    //     }
+    // if (e.keyCode == 38) {
+    //         // pour bouger le tireur vers le haut
+    //         if (x > 200) {
+    //             position = x;
+    //             tireurElement.classList.remove('tireur');
+    //             x -= 20;
+    //             document.getElementById('case-' + x).classList.add('tireur');
+    //             position = x;
+    //             console.log(position)
+    //         }
+    //     }
+    //     if (e.keyCode == 40) {
+    //         // pour bouger le tireur vers le bas
+    //         if (x < 221) {
+    //             position = x;
+    //             tireurElement.classList.remove('tireur');
+    //             x += 20;
+    //             document.getElementById('case-' + x).classList.add('tireur');
+    //             position = x;
+    //             console.log(position)
+    //         }
+    //     }
+    // });
+    
+    
+    // // fonction du laser pour le piou piou
+    // function laser() {
+    //     let i = x - 20;
+    //     let y = i - 40;
+    //     let bool = true;
+    //     if (bool === true) {
+    //         bool = false;
+    //         let intervalId = setInterval(function () {
+    //             if (i > -30 && y > -40) {
+    //                 if (document.getElementById('case-' + i).classList) {
+    //                     document.getElementById('case-' + i).classList.add('laser');
+    //                 }
+    //                 i -= 20;
+    //                 if (document.getElementById('case-' + y).classList) {
+    //                     document.getElementById('case-' + y).classList.remove('laser');
+    //                 }
+    //                 y -= 20;
+    //                 console.log(i, y)
+    //                 if (y == -20) {
+    //                     document.getElementById('case-' + y).classList.remove('laser');
+    //                 }
+    //             } else {
+    //                 clearInterval(intervalId);
+    //             }
+    //         }, 50);
+    //     }
+    // }
+    
+    // document.body.onkeyup = function (e) {
+    //     if (e.key == " " && e.code == "Space" &&  e.keyCode == 32) {
+    //         laser();
+    //         bool = false;
+    //     }
+    // }
+    
 
 
 
 
 
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // si la case est 11 21 31 ... alors c'est le coter gauche et si la case est 20 40 50... alors c'est droite 
 // mettre une classe a la div Pour générer le tableau de div si on continue le mouvement naturel de la chose il reviens a la ligne alors que nous on veux qu’il descende donc +1 +1 +1 +20 -1 -1 -1 0 +20 +1 +1 donc mettre un attribut a la div data-right = true donc tu fait +1 et dire que le comportement est different il faut qu’on dise que tout la colonne data - left = true  alors on fait +20 
